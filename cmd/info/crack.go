b4f80bbe958aa201f9a8bc2f23e8ab3d46bc53f0
Jordy <jordyderon@gmail.com>
2024-08-08 15:03:58 +0200

    small update

--- cmd/info/crack.go
+++ cmd/info/crack.go
@@ -14,7 +14,8 @@ import (
 )
 
 var (
-	query string
+	hashMethod string
+	hash	   string
 )
 
 var crackCmd = &cobra.Command{
@@ -27,15 +28,7 @@ var crackCmd = &cobra.Command{
 		info crack -q "md5 5f4dcc3b5aa765d61d8327deb882cf99"
 		info crack -q "sha256 5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8"`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("Cracking hash: %s\n\n", query)
-		queryArgs := strings.Split(query, " ")
-		if len(queryArgs) != 2 {
-			log.Fatal("Invalid query. Please provide a hash type and a hash value.")
-			return
-		}
-
-		hashMethod := queryArgs[0]
-		hash := queryArgs[1]
+		fmt.Printf("Cracking hash: %s, %s\n\n", hash, hashMethod)
 
 		passwords, err := ioutil.ReadFile("passwords.txt")
 		if err != nil {
@@ -73,7 +66,8 @@ var crackCmd = &cobra.Command{
 }
 
 func init() {
-	crackCmd.Flags().StringVarP(&query, "query", "q", "sha1 5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8", "Args for the crack command")
+	crackCmd.Flags().StringVarP(&hash, "hash", "x", "5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8", "Args for the crack command")
+	crackCmd.Flags().StringVarP(&hashMethod, "method", "m", "sha1", "Args for the crack command")
 
 	InfoCmd.AddCommand(crackCmd)
 }
